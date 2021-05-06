4e54a58bda2cbb762db9328719c37bed5db75f34
Sean Kelly <invertedrdm@gmail.com>
2021-05-06 18:41:20 -0400

    works for now I guess

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,13 +3,15 @@ import "./style.css";
 
 function Main(props) {
   return (
-          <div className="card col-md-6 col-sm-12" style={{width: '18rem'}}>
+          <div className="row">
+          <div className="card col-12" style={{width: '18rem'}}>
             <img className="card-img-top" src={props.screenshot} alt={"Screen shot of " + props.title} />
             <div className="card-body">
               <h5 className="card-title">{props.title}</h5>
-              <p className="card-text">{props.description}</p>
+              <p className="card-text">{props.desc}</p>
               <a href={props.deployed} target="_blank" className="btn btn-primary">View the live site!</a>
               <a href={props.repo} target="_blank" className="btn btn-primary">View the repo!</a>
+            </div>
             </div>
           </div>
   );
